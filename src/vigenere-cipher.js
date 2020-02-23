e48163c4f079fee2a0853c34df34c44c8af86e31
Andrei Abramchuk <stilegs@gmail.com>
2020-02-23 12:40:12 +0300

    temp

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,23 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+	encrypt(message, key) {
+		if (!message || !key) throw new Error();
+		const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		const maxLength = Math.max(message.length, message.length);
+		let result = '';
+		for (let i = 0; i < maxLength; i++) {
+			const m = alphabet.indexOf(message[message.length <= i ? i % message.length : i].toUpperCase());
+			console.log(m);
+			const k = key[key.length <= i ? i % key.length : i];
+			console.log(k);
+			result += m == -1 ? message[i] : alphabet[(m + alphabet.indexOf(k.toUpperCase())) % alphabet.length];
+			console.log(result);
+		}
+		return result;
+	}
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+	decrypt(encryptedMessage, key) {
+		if (!encryptedMessage || !key) throw new Error();
+	}
 }
 
 module.exports = VigenereCipheringMachine;
